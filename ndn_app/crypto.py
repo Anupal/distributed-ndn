3ea8ff97f83b105b1316b58c73f41098f3a6a8f4
Anupal Mishra <anupal.pm@gmail.com>
2023-11-13 12:57:26 +0000

    add b64 encoding decoding for signs and encrypted data

--- ndn_app/crypto.py
+++ ndn_app/crypto.py
@@ -6,6 +6,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography import x509
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.x509.oid import NameOID
+from base64 import b64encode, b64decode
 
 
 def generate_keys(key_size=2048):
@@ -16,23 +17,21 @@ def generate_keys(key_size=2048):
     return private_key, public_key
 
 
-def print_public_key(key_object):
-    pem_public_key = key_object.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    )
+def str_public_key(key_object):
+    return b64encode(
+        key_object.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+    ).decode("utf-8")
 
-    print(pem_public_key.decode("utf-8"))
 
-
-def print_private_key(key_object):
-    pem_private_key = key_object.private_bytes(
+def str_private_key(key_object):
+    return key_object.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption(),
-    )
-
-    print(pem_private_key.decode("utf-8"))
+    ).decode("utf-8")
 
 
 def encrypt_data(data, recipient_public_key):
@@ -44,19 +43,22 @@ def encrypt_data(data, recipient_public_key):
             label=None,
         ),
     )
-    return encrypted
+    return b64encode(encrypted).decode("utf-8")
 
 
 def decrypt_data(private_key, encrypted_data):
-    decrypted = private_key.decrypt(
-        encrypted_data,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None,
-        ),
-    )
-    return decrypted.decode("utf-8")
+    try:
+        decrypted = private_key.decrypt(
+            b64decode(encrypted_data),
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            ),
+        )
+        return decrypted.decode("utf-8")
+    except Exception:
+        return None
 
 
 def sign_data(private_key, data):
@@ -67,13 +69,13 @@ def sign_data(private_key, data):
         ),
         hashes.SHA256(),
     )
-    return signature
+    return b64encode(signature).decode("utf-8")
 
 
 def verify_signature(public_key, data, signature):
     try:
         public_key.verify(
-            signature,
+            b64decode(signature),
             data.encode("utf-8"),
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
@@ -85,41 +87,15 @@ def verify_signature(public_key, data, signature):
         return False
 
 
+def get_public_key_from_string(public_key_string):
+    return serialization.load_pem_public_key(
+        b64decode(public_key_string), backend=default_backend()
+    )
+
+
 # function to load a certificate that is commonly used across the network
 def load_certificate(file_path):
     with open(file_path, "rb") as f:
         cert_data = f.read()
     certificate = load_pem_x509_certificate(cert_data, default_backend())
     return certificate
-
-
-if __name__ == "__main__":
-    pvk, pbk = generate_keys()
-    _, pbk2 = generate_keys()
-
-    print("PRIVATE KEY")
-    print_private_key(pvk)
-
-    print("PUBLIC KEY")
-    print_public_key(pbk)
-
-    s = "anupal"
-    print("Source string:", s)
-
-    e = encrypt_data(s, pbk)
-    print("Encrypted:", e)
-
-    d = decrypt_data(pvk, e)
-    print("Decrypted:", d)
-
-    e2 = encrypt_data(s, pbk2)
-    try:
-        d2 = decrypt_data(pvk, e2)
-        print("Decrypted:", d2)
-    except ValueError:
-        print("Decryption failed!")
-
-    ss = sign_data(pvk, s)
-    print("Signed:", ss)
-
-    print("Verify sign:", verify_signature(pbk, s, ss))
