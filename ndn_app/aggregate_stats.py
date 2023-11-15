53e90e6823f8a15aeeb6e7344e93747c9263136a
Anupal Mishra <anupal.pm@gmail.com>
2023-11-15 23:32:32 +0000

    loop delays to avoid file read errors

--- ndn_app/aggregate_stats.py
+++ ndn_app/aggregate_stats.py
@@ -1,6 +1,7 @@
 import os
 import json
 import sys
+from time import sleep
 
 
 DIRECTORY_PATH = "stats"
@@ -16,16 +17,25 @@ def main():
             if os.path.isfile(os.path.join(DIRECTORY_PATH, f))
         ]
 
+        failure = False
         for file_name in files:
             file_path = os.path.join(DIRECTORY_PATH, file_name)
-            with open(file_path, "r") as file:
-                content = file.read()
-                content_dict = json.loads(content)
-                key = next(iter(content_dict))
-                file_contents[key] = content_dict[key]
+            try:
+                with open(file_path, "r") as file:
+                    content = file.read()
+                    content_dict = json.loads(content)
+                    key = next(iter(content_dict))
+                    file_contents[key] = content_dict[key]
+            except:
+                failure = True
+                break
+        if failure:
+            sleep(0.5)
+            continue
 
         with open(sys.argv[1], "w") as file:
             file.write(json.dumps(file_contents, indent=2))
+        sleep(0.5)
 
 
 if __name__ == "__main__":
