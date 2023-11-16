068b3f30adb0dc60eb94031f0b2ca55a59528f86
Anupal Mishra <anupal.pm@gmail.com>
2023-11-16 01:23:01 +0000

    CLI CMD driven UI

--- ndn_app/main.py
+++ ndn_app/main.py
@@ -1,180 +1,193 @@
-import sys
+import os
 from node import Node
 import time
 import constants
 import multiprocessing
+import sys
 from prettytable import PrettyTable
 
 
-def display_menu():
-    print("\nWelcome to NDN text console. Please choose an option")
+def clear_screen():
+    if os.name == "nt":
+        os.system("cls")  # For Windows
+    else:
+        os.system("clear")
+
+
+def show_help():
     table = PrettyTable()
-    table.field_names = ["Option", "Description"]
-    table.align["Option"] = "l"
+    table.field_names = ["Command", "Description"]
+    table.align["Command"] = "l"
     table.align["Description"] = "l"
 
-    table.add_row(["0", "Print menu"])
-    table.add_row(["1", "Print all nodes on this Pi"])
-    table.add_row(["2", "Print Node FIB"])
-    table.add_row(["3", "Print Node PIT"])
-    table.add_row(["4", "Print Node Counters"])
-    table.add_row(["5", "Send interest packet"])
-    table.add_row(["6", "Print member private key"])
-    table.add_row(["7", "Print node public key"])
-    table.add_row(["8", "Print node private key"])
-    table.add_row(["9", "Print last 10 packets"])
-    table.add_row(["10", "Print k-nearest"])
-    table.add_row(["11", "Pause node"])
-    table.add_row(["12", "Unpause node"])
-    print(table)
-    print("Note: Give -1 as an option to exit from any menu.\n")
+    table.add_row(["help", "Show all commands"])
+    table.add_row(["show all nodes", "Print all nodes on this Pi"])
+    table.add_row(["show fib <node>", "Print Node neighbors"])
+    table.add_row(["show pit <node>", "Print Node pending interests"])
+    table.add_row(["show counters <node>", "Print Node packet Counters"])
+    table.add_row(["send interest <node> <data-address>", "Send interest packet"])
+    table.add_row(["show key member <node>", "Print member private key"])
+    table.add_row(["show key gateway <node>", "Print member private key"])
+    table.add_row(["show key priv <node>", "Print node public key"])
+    table.add_row(["show key pub <node>", "Print node private key"])
+    table.add_row(["show packets <node>", "Print last 10 packets"])
+    table.add_row(["show knn <node>", "Print k-nearest"])
+    table.add_row(["show state <node>", "Print current state of node"])
+    table.add_row(["show gateway", "Print current state of node"])
+    table.add_row(["pause <node>", "Pause node"])
+    table.add_row(["unpause <node>", "Unpause node"])
+    print("\nSUPPORTED COMMANDS\n")
+    print(table, "\n")
 
 
-def pause_node(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "stop_comms", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
+def parse_input_prefix2(prefix, user_input, nodes):
+    try:
+        user_input = user_input.replace(prefix, "")
+        label, data_address = user_input.split(" ")[:2]
+        label = int(label)
+        if label in nodes:
+            return label, data_address
+        else:
+            print(f"Node {label} not found on this Pi!")
+            return None, None
+    except:
+        print("Invalid input!")
+        return None, None
 
 
-def unpause_node(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "start_comms", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
+def parse_input_prefix(prefix, user_input, nodes):
+    try:
+        label = int(user_input.replace(prefix, ""))
+        if label in nodes:
+            return label
+        else:
+            print(f"Node {label} not found on this Pi!")
+    except:
+        print("Invalid input!")
 
 
-def display_all_nodes(nodes):
-    print("Running nodes: ", end="")
-    for node in nodes:
-        print(node, end=", ")
-    print()
+def loop(nodes):
+    user_input = ""
+    while user_input != "exit":
+        sleep_duration = 1
+
+        try:
+            user_input = input("> ")
+
+            if user_input == "clear":
+                clear_screen()
+                sleep_duration = 0
+
+            if user_input == "help":
+                show_help()
+                sleep_duration = 0
+
+            elif user_input == "show all nodes":
+                print()
+                for node in nodes:
+                    print(node, end=" ")
+                print()
+                sleep_duration = 0
+
+            elif user_input == "show gateway":
+                if constants.GW_NODE_LABEL in nodes:
+                    nodes[constants.GW_NODE_LABEL].mgmt.put(
+                        {"call": "print_gateway", "args": ()}
+                    )
+                else:
+                    print("Gateway is not running on this Pi!")
+
+            elif "show fib " in user_input:
+                label = parse_input_prefix("show fib ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_fib", "args": ()})
+
+            elif "show pit " in user_input:
+                label = parse_input_prefix("show pit ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_pit", "args": ()})
+
+            elif "show knn " in user_input:
+                label = parse_input_prefix("show knn ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_knn", "args": ()})
+
+            elif "show counters " in user_input:
+                label = parse_input_prefix("show counters ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_counters", "args": ()})
+
+            elif "show packets " in user_input:
+                label = parse_input_prefix("show packets ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_last_10", "args": ()})
+                    sleep_duration = 1
+
+            elif "show key member " in user_input:
+                label = parse_input_prefix("show key member ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put(
+                        {"call": "print_member_private_key", "args": ()}
+                    )
+
+            elif "show key gateway " in user_input:
+                label = parse_input_prefix("show key gateway ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put(
+                        {"call": "print_gateway_private_key", "args": ()}
+                    )
+
+            elif "show key priv " in user_input:
+                label = parse_input_prefix("show key priv ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_private_key", "args": ()})
+
+            elif "show key pub " in user_input:
+                label = parse_input_prefix("show key pub ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_public_key", "args": ()})
+
+            elif "pause " in user_input:
+                label = parse_input_prefix("pause ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "stop_comms", "args": ()})
+
+            elif "unpause " in user_input:
+                label = parse_input_prefix("unpause ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "start_comms", "args": ()})
+
+            elif "show state " in user_input:
+                label = parse_input_prefix("show state ", user_input, nodes)
+                if label != None:
+                    nodes[label].mgmt.put({"call": "print_state", "args": ()})
+
+            elif "send interest " in user_input:
+                label, data_address = parse_input_prefix2(
+                    "send interest ", user_input, nodes
+                )
+                if label != None:
+                    nodes[label].mgmt.put(
+                        {"call": "send_interest_packet", "args": (data_address, 1)}
+                    )
+                    sleep_duration = 2
+            else:
+                sleep_duration = 0
+
+            time.sleep(sleep_duration)
+        except KeyboardInterrupt:
+            ...
+
+    for node_id in nodes:
+        nodes[node_id].terminate()
+        nodes[node_id].join()
 
 
-def display_fib(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_fib", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_pit(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_pit", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_knn(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_knn", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_counters(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_counters", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_last_10(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_last_10", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_member_private_key(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_member_private_key", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_private_key(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_private_key", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def display_public_key(nodes):
-    index = input("    Enter node index: ")
-    if "-1" == index:
-        return
-
-    index = int(index)
-    if index in nodes:
-        nodes[index].mgmt.put({"call": "print_public_key", "args": ()})
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def send_interest_packet(nodes):
-    index = input("    Enter node index: ")
-
-    data_address = input("    Enter data address: ")
-    retry = input("    Enter retry index: ")
-    if "-1" in (index, retry):
-        return
-    index, retry = int(index), int(retry)
-    if index in nodes:
-        nodes[index].mgmt.put(
-            {"call": "send_interest_packet", "args": (data_address, retry)}
-        )
-    else:
-        print(f"Node {index} not found on this Pi!")
-
-
-def main():
+if __name__ == "__main__":
     args = sys.argv
 
     if len(args) < 2:
-        print("Format: python3 ndn_app/main.py <rpi>")
+        print("Format: python3 main.py <rpi>")
         exit(1)
 
     rpi = int(args[1])
@@ -209,51 +222,6 @@ def main():
         nodes[i] = node
         node.start()
 
-    # Start menu loop
-
-    display_menu()
-    while True:
-        sleep_duration = 2
-        choice = input("> ")
-
-        if choice == "0":
-            display_menu()
-            sleep_duration = 0
-        elif choice == "1":
-            display_all_nodes(nodes)
-            sleep_duration = 0
-        elif choice == "2":
-            display_fib(nodes)
-        elif choice == "3":
-            display_pit(nodes)
-        elif choice == "4":
-            display_counters(nodes)
-        elif choice == "5":
-            send_interest_packet(nodes)
-        elif choice == "6":
-            display_member_private_key(nodes)
-        elif choice == "7":
-            display_public_key(nodes)
-        elif choice == "8":
-            display_private_key(nodes)
-        elif choice == "9":
-            display_last_10(nodes)
-        elif choice == "10":
-            display_knn(nodes)
-        elif choice == "11":
-            pause_node(nodes)
-        elif choice == "12":
-            unpause_node(nodes)
-        elif choice == "-1":
-            for node_id in nodes:
-                nodes[node_id].terminate()
-                nodes[node_id].join()
-            exit()
-        else:
-            sleep_duration = 0
-
-        time.sleep(sleep_duration)
-
-
-if __name__ == "__main__":
-    main()
+    print("\nWelcome to Medical Sensor Network - NDN.\n")
+    show_help()
+    loop(nodes)
